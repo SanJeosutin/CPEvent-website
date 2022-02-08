460563523c760a46f91dcc7f54d27f02880f82ff
Justin San <santosojustin@gmail.com>
2022-02-08 21:34:00 +1100

    Create dashboard.php

---
+++ app/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+include('reuseable/header.php');
+include('reuseable/navbar.php');
+?>
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container py-4">
+        <h1 class="display-4">Hi Jury Quackington!</h1>
+        <p class="lead">Welcome to Chill & Play <?php echo date('Y') ?> Events!</p>
+        <br>
+
+    </div>
+</div>
+
+<?php include('reuseable/footer.php') ?>
